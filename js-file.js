73bb5326da7262a761c9617cae0e6eeb74bc0fce
Hossain Ahmed <hossainahmed2002@gmail.com>
2024-01-01 02:08:44 +0000

    operator buttons
    when operator button is clicked, store first result as first num and operation is stored

--- js-file.js
+++ js-file.js
@@ -15,18 +15,34 @@ let firstNum = ""
 let secoundNum = ""
 let operator
 
+const operation = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+};
+
 function operate(x, y, operator){
     return operator(x,y)
 }
 
 const result = document.getElementById("result")
 const digits = document.querySelectorAll("button.digit");
-const operators = document.querySelectorAll("button.opertor");
+const operators = document.querySelectorAll("button.operator");
 
 
 digits.forEach((digit) =>{
     digit.addEventListener("click", function() {
-        firstNum = firstNum + digit.textContent
-        result.textContent = firstNum
+        secoundNum = secoundNum + digit.textContent
+        result.textContent = secoundNum
     })
+})
+
+operators.forEach((operareBtn) =>{
+        operareBtn.addEventListener( "click",function(){
+            firstNum = result.textContent
+            secoundNum = ""
+            result.textContent = "0"
+            operator = operation[operareBtn.textContent]
+        })
 })
